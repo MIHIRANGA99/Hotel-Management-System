aa0984644afe4c2dc7974ad2f69dc259d867adf4
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-03-18 00:43:09 +0530

    created routes for components

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ButtonComponent from './Screens/ButtonComponent';
 import Home from './Screens/Home';
 import Login from './Screens/Login';
 import Register from './Screens/Register';
 import SampleCRUD from './Screens/SampleCRUD';
+import TextFieldComponent from './Screens/TextFieldComponent';
 import Welcome from './Screens/Welcome';
 
 export default function App() {
@@ -17,6 +19,10 @@ export default function App() {
         <Stack.Screen name='Register' component={Register} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Home' component={Home} />
+
+        {/* Components */}
+        <Stack.Screen name='Button' component={ButtonComponent} />
+        <Stack.Screen name='TextField' component={TextFieldComponent} />
 
         {/* remove this when project has done */}
         <Stack.Screen name='CRUD' component={SampleCRUD} />
