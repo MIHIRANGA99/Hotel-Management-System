fa77543add1866ba59b1ca5a468b6cad9e2f2cfb
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-03-19 13:19:26 +0530

    rearranged home and welcome

--- Screens/Home.js
+++ Screens/Home.js
@@ -13,11 +13,6 @@ const Home = ({navigation}) => {
   return (
     <View>
         <Text>Hello, {user}</Text>
-
-        <Button onPress={() => navigation.navigate("CRUD")} title="View My Sample Crud" />
-        <Text style = {{fontSize: 24, textAlign: 'center', marginVertical: 24}}>View Components</Text>
-        <Button onPress={() => navigation.navigate("Button")} title="Button Component" />
-        <Button onPress={() => navigation.navigate("TextField")} title="Text Field Component" />
     </View>
   )
 }
