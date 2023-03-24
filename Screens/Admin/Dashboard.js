d80b465b7812ef7cceb696523f276fb7fb3d6b66
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-03-24 10:17:18 +0530

    formatted and added logo

--- Screens/Admin/Dashboard.js
+++ Screens/Admin/Dashboard.js
@@ -1,26 +1,55 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import Button from '../../Components/Button/Button'
+import React from "react";
+import { Image } from "react-native";
+import { Text, View } from "react-native";
+import Button from "../../Components/Button/Button";
 
-const Dashboard = ({navigation}) => {
+const Dashboard = ({ navigation }) => {
   return (
-    <View style = {{flex: 1, justifyContent: 'space-between', padding: 12}}>
-        <View>
-            <Text>Status</Text>
-        </View>
-        <View style = {{marginBottom: "3%"}}>
-            <Button extraStyles={{marginTop: 12}} title='Manage Bookings' />
-            <Button onClick={() => navigation.navigate("ManageHotels")} extraStyles={{marginTop: 12}} title='Manage Hotels' />
-            <Button onClick={() => navigation.navigate("AddHotel")} extraStyles={{marginTop: 12}} title='Add New Hotel' />
-            <Button onClick={() => navigation.navigate("AddNewFood")} extraStyles={{marginTop: 12}} title='Add New Food Menu' />
+    <View style={{ flex: 1, justifyContent: "space-between", padding: 12 }}>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          style={{ width: 300, height: "100%", resizeMode: "contain" }}
+          source={{
+            uri: "https://firebasestorage.googleapis.com/v0/b/hotel-management-native.appspot.com/o/Indigo%20Hotels.png?alt=media&token=640525c2-6948-4d03-a18b-8988e25059a4",
+          }}
+        />
+      </View>
+      <View style={{ marginBottom: "3%" }}>
+        <Button extraStyles={{ marginTop: 12 }} title="Manage Bookings" />
+        <Button
+          onClick={() => navigation.navigate("ManageHotels")}
+          extraStyles={{ marginTop: 12 }}
+          title="Manage Hotels"
+        />
+        <Button
+          onClick={() => navigation.navigate("AddHotel")}
+          extraStyles={{ marginTop: 12 }}
+          title="Add New Hotel"
+        />
 
-            <Button onClick={() => navigation.navigate("ManageAllFoodsMenu")} extraStyles={{marginTop: 12}} title='All Food Menu' />
-            
+        <Button
+          onClick={() => navigation.navigate("AddNewFood")}
+          extraStyles={{ marginTop: 12 }}
+          title="Add New Food Menu"
+        />
 
-            <Button extraStyles={{marginTop: 12}} title='Manage Food' />
-        </View>
+        <Button
+          onClick={() => navigation.navigate("ManageAllFoodsMenu")}
+          extraStyles={{ marginTop: 12 }}
+          title="All Food Menu"
+        />
+
+        <Button extraStyles={{ marginTop: 12 }} title="Manage Food" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
