ba788f7d349a81f8dee9903225c9b5be75f016b7
Sandumini Weerasinghe <sandumini.weerasinghe@pearson.com>
2023-03-24 16:03:13 +0530

    Adding changes to Cart.js page

--- Screens/Client/Cart.js
+++ Screens/Client/Cart.js
@@ -18,26 +18,25 @@ const Cart = () => {
 
     const handleDelete = (docId) => {
         deleteFromCollection("Cart1", docId, () => {
-        
-        readCart( );
-    alert("Successfully Removed added foods")
+            readCart( );
+            alert("Successfully Removed added foods")
         }, (error) => {
-        console.error(error);
+            console.error(error);
         })
     }
     return (
         <View>
-        <Text style={{ fontSize: 38, fontWeight: "600" }}> #Your Cart#</Text>
-        {cartItems.map((cartItem) => (
-            <View key={cartItem.id}>
-            <Image source={{ uri: cartItem.url }} style={{ width: 420, height: 150 }} />
-            <Text style={{ fontSize: 12, fontWeight: "600" }}>Food ID: {cartItem.id}</Text>
-            <Text style={{ fontSize: 12, fontWeight: "600" }}>Your ID: {cartItem.userID}</Text>
-            <Text style={{ fontSize: 18, fontWeight: "600" }}>Food Name: {cartItem.name}</Text>
-            <Text style={{ fontSize: 18, fontWeight: "600" }}>Price:{cartItem.price}</Text>
-            <Button title="Remove" onPress={() => handleDelete(cartItem.id)} />
-            </View>
-        ))}
+            <Text style={{ fontSize: 38, fontWeight: "600" }}> #Your Cart#</Text>
+            {cartItems.map((cartItem) => (
+                <View key={cartItem.id}>
+                <Image source={{ uri: cartItem.url }} style={{ width: 420, height: 150 }} />
+                <Text style={{ fontSize: 12, fontWeight: "600" }}>Food ID: {cartItem.id}</Text>
+                <Text style={{ fontSize: 12, fontWeight: "600" }}>Your ID: {cartItem.userID}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "600" }}>Food Name: {cartItem.name}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "600" }}>Price:{cartItem.price}</Text>
+                <Button title="Remove" onPress={() => handleDelete(cartItem.id)} />
+                </View>
+            ))}
         </View>
     );
 };
