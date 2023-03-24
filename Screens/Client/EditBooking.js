f6b81be6a8656820d2ee58b76a314326dfa1964e
chethuuu <it20065294@my.sliit.lk>
2023-03-24 22:21:14 +0530

    modify update booking

--- Screens/Client/EditBooking.js
+++ Screens/Client/EditBooking.js
@@ -65,7 +65,7 @@ const EditBooking = ({ navigation, route }) => {
         <View style={{ paddingBottom: 12 }}>
           <Text style={{ fontSize: 16 }}>{hotel.location}</Text>
           <Text style={{ fontSize: 16 }}>{hotel.rooms} Bed Rooms</Text>
-          <Text style={{ fontSize: 16 }}>LKR {hotel.amount} Per Day</Text>
+          <Text style={{ fontSize: 16 }}>LKR: {hotel.amount} Per Day</Text>
         </View>
         <View>
           <Text style={{ fontSize: 18, fontWeight: "600", marginBottom: 12 }}>
